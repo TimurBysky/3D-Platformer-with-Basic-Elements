f7a34bf051f0844c6712364e0443321413eea1e2
Timur_Bysky <Timurbysky@gmail.com>
2025-04-10 16:40:40 +0300

    Создание машины состояний

--- player.gd
+++ player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody3D
 
 @onready var animation_player = get_node("godotman/AnimationPlayer")
-@onready var model = get_node("godotman/godot_rig/Skeleton3D/godot_mesh")  # Предполагая, что модель - это дочерний узел
+@onready var model = get_node("godotman/godot_rig/Skeleton3D/godot_mesh") 
+
+enum State { IDLE, RUN, ATTACK, DEAD }
+
 
 # Настройки
 var speed = 3.0
@@ -22,16 +25,16 @@ func add_coin():
 	emit_signal("coin_collected", coins)
 
 func _physics_process(delta):
-	if not is_alive: return
+	if not is_alive: 
+		set_state(State.DEAD)
+		return
 	
 	# Гравитация
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 		
-	# Прыжок
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
-		
 	# Движение
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -39,23 +42,30 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
+		set_state(State.RUN)
 		
-		# Плавный поворот в сторону движения
+		# Поворот модели
 		var target_angle = atan2(direction.x, direction.z)
 		model.rotation.y = lerp_angle(model.rotation.y, target_angle, delta * rotation_speed)
-		
-		animation_player.play("run", 0.3, 1.0)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
-		animation_player.play("idle", 0.3, 1.0)
+		set_state(State.IDLE)
 	
 	move_and_slide()
+	
+func set_state(new_state: State):
+	match new_state:
+		State.IDLE:
+			animation_player.play("idle", 0.3)
+		State.RUN:
+			animation_player.play("run", 0.3)
+		State.DEAD:
+			animation_player.play("die", 0.3)
 
 func _on_death_zone_entered():
 	is_alive = false
-	animation_player.play("die", 0.3, 1.0)
 	await get_tree().create_timer(1.0).timeout
 	position = Vector3(0, 5, 0)
 	is_alive = true
-	animation_player.play("idle")
+	set_state(State.IDLE)
